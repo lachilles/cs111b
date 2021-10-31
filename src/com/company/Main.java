3f9e0009109f711a2e7cae16b3da73544a70a82a
Lianne Achilles <lianne.achilles@udemy.com>
2021-10-31 13:51:06 -0700

    Add Names and Ages Program CS 111B Assignment 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
-/* Programming Exercise for Chapters 4-6: Days Out, for CS 111B
+/* Names and Ages Program CS 111B Assignment 2
    Written by Lianne Achilles
-   Calculates the average number of days a company's employees are absent
+   Determines, for two people, if neither, one, or both of them are between 18-65
 */
 package com.company;
 import java.util.Scanner;
@@ -9,75 +9,59 @@ public class Main
 {
     static Scanner keyIn = new Scanner(System.in);
 
+    // would love a person class with name and age properties
+
+    // get name and age inputs (no args)
+
+    // check if age is in 18-65 range (takes name1, name2, age1, age2 args)
+    // return Neither, Both, or one
     public static void main(String args[])
     {
-        int   employees,            // The number of employees
-                daysAbsent;           // Total days misssed by all employees
+        // define variables
+        String name1, name2;          // names of person1 and person2
+        int age1, age2;               // ages of person1 and person2
+        boolean workingAge1, workingAge2;
 
-        employees = getNumEmps();
-        daysAbsent = getDaysMissed(employees);
-        showAvgDaysMissed(employees, daysAbsent);
+        // get name and age inputs
+        System.out.println("Please enter first person's name: ");
+        name1 = keyIn.nextLine();
+        System.out.printf ("Please enter %s's age: ", name1);
+        age1 = keyIn.nextInt();
+        keyIn.nextLine();  // read the newline
+        // check if age is in 18-65 range
+        workingAge1 = checkAgeRange(age1);
+
+        System.out.println("Please enter second person's name: ");
+        name2 = keyIn.nextLine();
+        System.out.printf ("Please enter %s's age: ", name2);
+        age2 = keyIn.nextInt();
+        keyIn.nextLine();  // read the newline
+        // check if age is in 18-65 range
+        workingAge2 = checkAgeRange(age2);
+
+        if (workingAge1 && workingAge2) {
+            System.out.println("Both people are between 18 and 65.");
+        } else if (!(workingAge1 || workingAge2)) {
+            System.out.println("Neither person is between 18 and 65.");
+        } else if (!workingAge1 && workingAge2) {
+            System.out.printf("%s is between 18 and 65, but %s is not.", name2, name1);
+        } else if (workingAge1 && !workingAge2) {
+            System.out.printf("%s is between 18 and 65, but %s is not.", name1, name2);
+        }
     } // End of main function
 
     //*******************************************************************
-    // Function getNumEmps. This function asks the user to enter        *
-    // the number of employees in the company. Validation is performed  *
-    // to verify at least one employee, the value entered is            *
-    // then returned to function main.                                  *
-    //*******************************************************************
-    static int getNumEmps()
-    {
-        Scanner scan = new Scanner (System.in);
-        int employees;
-
-        System.out.print("How many employees are there in the company? ");
-        employees = scan.nextInt();
-        if (employees > 0)
-        {
-            return employees;
-        }
-        else
-        {
-            System.out.println("The number of employees must be one or greater. Please re-enter:");
-        }
-
-        return employees;
-    } // End of function getNumEmps
-
-    //*******************************************************************
-    // Function daysMissed. Accepts as an argument the number of        *
-    // employees in the company. User is asked to enter the             *
-    // number of days missed by each employee. Validation performed to  *
-    // confirm negative number is not entered. The total days missed    *
-    // by all employees is returned.                                    *
+    // Function checkAgeRange accepts an age and returns a boolean on   *
+    // whether or not the age is within 18-65 years old.                *
     //*******************************************************************
 
-    public static int getDaysMissed(int employees)
-    {
-        int days_missed, emp_no=1;
-        int total_days_missed = 0;
+    public static boolean checkAgeRange(int age) {
 
-        Scanner scan = new Scanner (System.in);
+        boolean workingAge;
 
-        do {
-            System.out.printf("Days missed by employee # %d : ", emp_no);
-            days_missed = scan.nextInt();
-            for(emp_no=1; emp_no<=employees; emp_no++)
-                total_days_missed += days_missed;
-        }while(emp_no<=employees);
+        workingAge = age < 65 && age >= 18;
 
-        return total_days_missed;
-    }
+        return workingAge;
+    } // End of function checkAgeRange
 
-    //*******************************************************************
-    // Function showAvgDaysMissed. Takes two arguments: the number of
-    // employees in the company and the total number of days absent for
-    // all employees during the year. Prints the average number of days
-    // missed per employee.
-
-    public static void showAvgDaysMissed(int employees, int daysAbsent) {
-        double avg_days_missed;
-        avg_days_missed = (double) daysAbsent / employees;
-        System.out.printf("The average number of days missed per employee is %.1f ", avg_days_missed);
-    }
 } // End of class Main
