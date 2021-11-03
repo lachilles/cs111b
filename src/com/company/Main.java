c016885f3f8b5028a1b93cce41cf30aa7cd93925
Lianne Achilles <lianne.achilles@udemy.com>
2021-11-02 18:18:03 -0700

    Add assignment 3: Analyze Phrase program

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,97 +1,101 @@
-/* CS 111B Exercise for Chapter 7: NameArray
-   Written by Lianne Achilles
-   This program asks the user for number of names they would like to input. Then asks them to input
-   the names. Then displays the names in reverse order and prints the longest name.
+/* Lianne Achilles
+   CS 111B Assignment 3
+   Count Words and display longest word
 */
 package com.company;
 import java.util.Scanner;
 
 public class Main
 {
-    static Scanner keyIn = new Scanner(System.in);
+    public static void main(String[] args) {
+        String phrase;
+        int numWords;
 
-    public static void main(String args[]) {
-//        // define variables
-        String names[];
-        String longestName;
-        int size, i;
+        phrase = inputPhrase();
 
-        System.out.print("How many names do you want to enter? ");
-        size = keyIn.nextInt();
-        keyIn.nextLine();  // read the newline
+        numWords = analyzePhrase(phrase);
 
-        names = new String[size];
+        System.out.println("Your phrase has " + numWords + " words in it.");
+        System.out.println();
+    }  // end of main function
 
-        // create the array
-        for (i=0; i < size; i++) {
-            System.out.println("Enter a name: ");
-            names[i] = keyIn.nextLine();
+    /**
+     * inputPhrase method
+     * Ask the user to input a phrase, and make sure it is at least 5 characters long.
+     * (Uses recursion to make the user input a new phrase if it is too short.)
+     *
+     * @return the phrase
+     */
+
+    private static String inputPhrase() {
+        Scanner keyIn = new Scanner(System.in);
+        System.out.print("Please enter a phrase all on one line, \n" +
+                "with just one space separating each word, \n" +
+                "and no spaces at the beginning or end of the phrase. \n");
+        String phrase = keyIn.nextLine();
+        if (phrase.length() < 5) {
+            System.out.print("Your phrase must contain at least 5 characters. \n");
+            phrase = inputPhrase();
         }
-        // print array in reverse order
-        System.out.println("Here are the names you entered, in reverse order:\n");
-        for (i=names.length - 1; i >= 0; i--)
-            System.out.print(names[i] + "\n");
+        return phrase;
+    }  // end of inputPhrase
 
-        //
-        longestName = getLongestName(names);
-        System.out.printf("\nHere is the longest name, with %d characters: %s",
-                longestName.length(), longestName);
-    } // End of main function
+    /**
+     * analyzePhrase method
+     * Count the number of words in a given string, and determine its longest word.
+     * Assume the string is well formed and doesn't have leading
+     * or trailing spaces, or multiple spaces in a row.
+     * Return the number of words.
+     * Output the longest word on the screen (console).
+     *
+     * @param phrase to analyze
+     * @return number of words in str
+     */
 
-    //*******************************************************************
-    // Function getLongestName accepts an array of names and returns    *
-    // the longest one.                                                 *
-    //*******************************************************************
-
-    static String getLongestName(String names[]) {
-
-        int index = 0;
-        int nameLength = names[0].length();
-
-        for(int i=1; i< names.length; i++) {
-            if(names[i].length() > nameLength) {
-                index = i; nameLength = names[i].length();
+    private static int analyzePhrase(String phrase) {
+        int i, len, temp_c = 0, temp_d = 0, numWords = 1;
+        int maxWordLength = 0;
+        String longestWord = null;
+        if (phrase.indexOf(' ') == -1) {  // handle the case of one
+            // word phrase like "Hello"
+            longestWord = phrase;
+        } else {
+            for (i = 0; i < phrase.length(); i++) {
+                char letter = phrase.charAt(i);
+                if (letter == ' ' || letter == '.') {
+                    temp_d = i;  // get ending index of word
+                    String curWord = phrase.substring(temp_c, temp_d);
+                    len = temp_d - temp_c;  // subtract beginning index from ending to get length of
+                    // word
+                    if (len > maxWordLength) {
+                        longestWord = curWord; // if length of current word
+                        // is longer than the maxWordLength previously seen, then replace the longestWord
+                        maxWordLength = longestWord.length(); // set len to be the new
+                    }
+                    temp_c = temp_d + 1;  // set starting index of new word to ending index of last word
+                    numWords += 1;  // increment wordcounter
+                }
             }
         }
-        return names[index];
-    } // End of function getLongestName
+        System.out.printf("\nThe longest word in your phrase is %s with %d characters. ",
+                longestWord, longestWord.length());
+        return numWords;
+    }  // end of analyzePhrase  function
+} // end of main class
 
-} // End of class Main
+/* Sample Output:
 
-///Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java -javaagent:/Applications/IntelliJ IDEA 2.app/Contents/lib/idea_rt.jar=54877:/Applications/IntelliJ IDEA 2.app/Contents/bin -Dfile.encoding=UTF-8 -classpath /Users/lianneachilles/Code/cs111b/out/production/cs111b com.company.Main
-//How many names do you want to enter? 3
-//Enter a name:
-//Jean
-//Enter a name:
-//Lianne
-//Enter a name:
-//Odie
-//Here are the names you entered, in reverse order:
-//
-//Odie
-//Lianne
-//Jean
-//
-//Here is the longest name, with 6 characters: Lianne
-//Process finished with exit code 0
+Please enter a phrase all on one line,
+with just one space separating each word,
+and no spaces at the beginning or end of the phrase.
+Barking Up The Wrong Tree
 
+The longest word in your phrase is Barking with 7 characters. Your phrase has 5 words in it.
 
-///Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/java -javaagent:/Applications/IntelliJ IDEA 2.app/Contents/lib/idea_rt.jar=54988:/Applications/IntelliJ IDEA 2.app/Contents/bin -Dfile.encoding=UTF-8 -classpath /Users/lianneachilles/Code/cs111b/out/production/cs111b com.company.Main
-//How many names do you want to enter? 4
-//Enter a name:
-//Bill
-//Enter a name:
-//Michiko
-//Enter a name:
-//Lianne
-//Enter a name:
-//Emi
-//Here are the names you entered, in reverse order:
-//
-//Emi
-//Lianne
-//Michiko
-//Bill
-//
-//Here is the longest name, with 7 characters: Michiko
-//Process finished with exit code 0
+Please enter a phrase all on one line,
+with just one space separating each word,
+and no spaces at the beginning or end of the phrase.
+You Can't Judge a Book By Its Cover
+
+The longest word in your phrase is Can't with 5 characters. Your phrase has 8 words in it.
+*/
